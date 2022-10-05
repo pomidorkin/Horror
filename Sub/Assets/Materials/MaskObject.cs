8d3a8322ea144e5fd62628ada00960612fbe465d
Pomidorkin <pbriuzgin@gmail.com>
2022-10-06 00:07:20 +0900

    Sadako added

---
+++ Sub/Assets/Materials/MaskObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskObject : MonoBehaviour
+{
+    public GameObject[] ObjMasked;
+    void Start()
+    {
+        for (int i = 0; i < ObjMasked.Length; i++)
+        {
+            ObjMasked[i].GetComponent<SkinnedMeshRenderer>().material.renderQueue = 3002;
+        }
+    }
+}
+
