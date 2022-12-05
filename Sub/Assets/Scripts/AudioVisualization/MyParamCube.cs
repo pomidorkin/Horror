479d94a7d2a8b8c5215244033d84af730bc7f863
Pomidorkin <pbriuzgin@gmail.com>
2022-12-05 16:18:44 +0900

    Corridor stage respawn bug fixed

--- Sub/Assets/Scripts/AudioVisualization/MyParamCube.cs
+++ Sub/Assets/Scripts/AudioVisualization/MyParamCube.cs
@@ -8,12 +8,14 @@ public class MyParamCube : MonoBehaviour
     [SerializeField] [Range(0f, 10f)] float animTriggerValue = 1.6f;
     private PlayerActions playerActions;
     private Door myChildDoor;
+    private GameManagerScript gameManager;
     float elapsedTime;
     float timeLimit = 0.1f;
     private void Start()
     {
         playerActions = FindObjectOfType<PlayerActions>();
         myChildDoor = GetComponentInChildren<Door>();
+        gameManager = FindObjectOfType<GameManagerScript>();
     }
     void Update()
     {
@@ -22,7 +24,7 @@ public class MyParamCube : MonoBehaviour
         {
             gameObject.GetComponent<Animator>().Play("SlammAnimation");
         }*/
-        if ( elapsedTime >= timeLimit && GetFMODSpectrumData.bandBuffer[band] > animTriggerValue && playerActions.lastLookedAtObject != myChildDoor.gameObject)
+        if ( elapsedTime >= timeLimit && GetFMODSpectrumData.bandBuffer[band] > animTriggerValue && playerActions.lastLookedAtObject != myChildDoor.gameObject && !gameManager.GetIsRespawningStage())
         {
             elapsedTime = 0;
             gameObject.GetComponent<Animator>().Play("SlammAnimation");
